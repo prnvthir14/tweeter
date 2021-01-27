fc7928e3f4b4e3e6cfb7a6dc0a59148384b392f9
prnvthir14 <prnvthir@gmail.com>
2021-01-27 19:47:27 +0000

    cleaned up files, commented out unnecssary code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,13 +4,13 @@
 
 $(document).ready(function() {
   
-
+  //access HTML by ID
   $('#tweet-text').on('keyup', function () {
     //this refers to text in texaarea
     let inputLength = ($(this).val()).length;
 
 
-    
+    //access HTML by class
     $('.counter').val(140 - inputLength)
 
     //
---
+++ public/scripts/tweets.js
@@ -0,0 +1,23 @@
+// //fake tweet data here:
+
+
+// //need to create a template article that will get stored in tweet-container section
+
+// //has hover state and should remove user name when 
+
+// //attmpt 1
+// $(document).ready(function() {
+  
+//   //access HTML by ID
+//   $(".tweet-container").hover(
+//     function() {
+//       $(this).addClass( "hover" );
+//     }, function() {
+//       $(this).removeClass( "hover" );
+//     }
+//   );
+
+// });
+
+
+// // $('.tweet-container').appened(/*function to create tweet object*/)
