3171e543161969aa43b80493719e08ec8cc5abb1
prnvthir14 <prnvthir@gmail.com>
2021-01-29 13:06:14 +0000

    function to show text area on click

---
+++ public/scripts/hideAndShowTypeBar.js
@@ -0,0 +1,12 @@
+//This file will be solely responsible for hiding and showing the 
+//typing bar 
+
+
+$(document).ready(function(){
+  //console.log('hi)
+  $("#type-tweet").click(function(){
+
+    $('#tweet-text').toggle();
+
+  });
+});
